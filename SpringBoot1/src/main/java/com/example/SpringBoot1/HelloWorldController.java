f3616f86c6b2940fe6f7eaf54e4daf95af8dd33e
lakshay <lakshay22csu100@ncuindia.edu>
2025-02-17 19:36:07 +0530

    Spring Boot Chapter-1

---
+++ SpringBoot1/src/main/java/com/example/SpringBoot1/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.SpringBoot1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+
+
+
+    @GetMapping(path = "/whatsup")
+    public String kyaHaalHai(){
+        return "Kya Haal Hai Bhai K?";
+    }
+}
---
+++ SpringBoot1/src/main/java/com/example/SpringBoot1/SpringBoot1Application.java
@@ -0,0 +1,16 @@
+package com.example.SpringBoot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// This annotation is very helpfull and does so much thing but now it is just telling it is the entry point to the application
+@SpringBootApplication
+
+public class 		SpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot1Application.class, args);
+	}
+
+}
